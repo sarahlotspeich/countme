34ac2f904e15561f93524166623cb522ac113924
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-06-15 10:03:38 -0400

    Wrap gold standard, naive, complete-case zic.reg() with tryCatch() for singularity

--- sims/run_sett_vary_zero_infl.R
+++ sims/run_sett_vary_zero_infl.R
@@ -55,6 +55,7 @@ run_sett_vary_zero_infl = function(eta0, sn = 35, nrep = 50) {
                    se_eta0_gs = NA,
                    se_eta1_gs = NA,
                    se_theta_gs = NA,
+                   msg_gs = NA,
                    beta0_n = NA,
                    beta1_n = NA,
                    eta0_n = NA,
@@ -65,6 +66,7 @@ run_sett_vary_zero_infl = function(eta0, sn = 35, nrep = 50) {
                    se_eta0_n = NA,
                    se_eta1_n = NA,
                    se_theta_n = NA,
+                   msg_n = NA,
                    beta0_cc = NA,
                    beta1_cc = NA,
                    eta0_cc = NA,
@@ -75,6 +77,7 @@ run_sett_vary_zero_infl = function(eta0, sn = 35, nrep = 50) {
                    se_eta0_cc = NA,
                    se_eta1_cc = NA,
                    se_theta_cc = NA,
+                   msg_cc = NA,
                    beta0_smle = NA,
                    beta1_smle = NA,
                    theta_smle = NA,
@@ -100,41 +103,92 @@ run_sett_vary_zero_infl = function(eta0, sn = 35, nrep = 50) {
     rdat = sim_data(eta0 = eta0)
 
     ## Fit gold standard model
-    gs_fit = zic.reg(
-      fmla = y ~ x1f | z,
-      data = rdat,
-      dist = "nbinom",
-      optimizer = "nlm"
+    gs_fit = tryCatch(
+      suppressWarnings(
+        zic.reg(
+          fmla = y ~ x1f | z,
+          data = rdat,
+          dist = "nbinom",
+          optimizer = "nlm"
+        )
+      ),
+      error = function(e) {
+        if (grepl("singular|solve.default", conditionMessage(e), ignore.case = TRUE)) {
+          warning("Zero-inflated model failed due to a singular system. ",
+                  "Consider refitting without zero inflation.")
+        } else {
+          warning("zic.reg failed with unexpected error: ", conditionMessage(e))
+        }
+        NULL
+      }
     )
 
     ### Save estimates to res
-    res[r, c("beta0_gs", "beta1_gs", "eta0_gs", "eta1_gs", "theta_gs")] = gs_fit$coef
-    res[r, c("se_beta0_gs","se_beta1_gs", "se_eta0_gs", "se_eta1_gs", "se_theta_gs")] = sqrt(diag(vcov(gs_fit)))
+    if (!is.null(new_fit)) {
+      res[r, c("beta0_gs", "beta1_gs", "eta0_gs", "eta1_gs", "theta_gs")] = gs_fit$coef
+      res[r, c("se_beta0_gs","se_beta1_gs", "se_eta0_gs", "se_eta1_gs", "se_theta_gs")] = sqrt(diag(vcov(gs_fit)))
+    } else {
+      res[r, "msg_gs"] = "zic.reg failed"
+    }
 
     ## Fit naive model
-    n_fit = zic.reg(
-      fmla = y ~ x1star | z,
-      data = rdat,
-      dist = "nbinom",
-      optimizer = "nlm"
+    n_fit = tryCatch(
+      suppressWarnings(
+        zic.reg(
+          fmla = y ~ x1star | z,
+          data = rdat,
+          dist = "nbinom",
+          optimizer = "nlm"
+        )
+      ),
+      error = function(e) {
+        if (grepl("singular|solve.default", conditionMessage(e), ignore.case = TRUE)) {
+          warning("Zero-inflated model failed due to a singular system. ",
+                  "Consider refitting without zero inflation.")
+        } else {
+          warning("zic.reg failed with unexpected error: ", conditionMessage(e))
+        }
+        NULL
+      }
     )
 
     ### Save estimates to res
-    res[r, c("beta0_n", "beta1_n", "eta0_n", "eta1_n", "theta_n")] = n_fit$coef
-    res[r, c("se_beta0_n","se_beta1_n", "se_eta0_n", "se_eta1_n", "se_theta_n")] = sqrt(diag(vcov(n_fit)))
+    if (!is.null(new_fit)) {
+      res[r, c("beta0_n", "beta1_n", "eta0_n", "eta1_n", "theta_n")] = n_fit$coef
+      res[r, c("se_beta0_n","se_beta1_n", "se_eta0_n", "se_eta1_n", "se_theta_n")] = sqrt(diag(vcov(n_fit)))
+    } else {
+      res[r, "msg_n"] = "zic.reg failed"
+    }
 
     ## Fit complete-case model
     rdat_cc = subset(rdat, !is.na(x1))
-    cc_fit = zic.reg(
-      fmla = y ~ x1 | z,
-      data = rdat_cc,
-      dist = "nbinom",
-      optimizer = "nlm"
+    cc_fit = tryCatch(
+      suppressWarnings(
+        zic.reg(
+          fmla = y ~ x1 | z,
+          data = rdat_cc,
+          dist = "nbinom",
+          optimizer = "nlm"
+        )
+      ),
+      error = function(e) {
+        if (grepl("singular|solve.default", conditionMessage(e), ignore.case = TRUE)) {
+          warning("Zero-inflated model failed due to a singular system. ",
+                  "Consider refitting without zero inflation.")
+        } else {
+          warning("zic.reg failed with unexpected error: ", conditionMessage(e))
+        }
+        NULL
+      }
     )
 
     ### Save estimates to res
-    res[r, c("beta0_cc", "beta1_cc", "eta0_cc", "eta1_cc", "theta_cc")] = cc_fit$coef
-    res[r, c("se_beta0_cc","se_beta1_cc", "se_eta0_cc", "se_eta1_cc", "se_theta_cc")] = sqrt(diag(vcov(cc_fit)))
+    if (!is.null(new_fit)) {
+      res[r, c("beta0_cc", "beta1_cc", "eta0_cc", "eta1_cc", "theta_cc")] = cc_fit$coef
+      res[r, c("se_beta0_cc","se_beta1_cc", "se_eta0_cc", "se_eta1_cc", "se_theta_cc")] = sqrt(diag(vcov(cc_fit)))
+    } else {
+      res[r, "msg_cc"] = "zic.reg failed"
+    }
 
     ## Fit SMLE model
     smle_fit = smle_nb(analysis_formula = y ~ x1,
